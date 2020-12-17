8415300ba7ca66c37c381b1026d9416ab2cbf657
Jonathan Getahun <kgetahun2@gmail.com>
2020-12-16 22:01:01 -0500

    adding env files

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
-mongoose.connect('mongodb+srv://jonathangetahun:jonathangetahun@cluster0.pabfy.mongodb.net/<dbname>?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+const config = require('./config/key');
+
+const { User } = require('./models/user');
+
+mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log('DB connected')
 }).catch(err => console.error(err))
 
+app.use(bodyParser.urlencoded({ extended : true}));
+app.use(bodyParser.json());
+app.use(cookieParser()); //parses cookies attached to the client request object
 
-app.get("/", (req, res) => {
-    res.send("hello world")
+
+app.post('/api/users/register', (req,res) => {
+    const user = new User(req.body);
+
+    user.save((err, userData) => {
+        if(err) return res.json({sucess:false, err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+
 })
 
 
