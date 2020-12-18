6c12e2f97fad874044cd8b4ee26d872d5b51c83d
Jonathan Getahun <kgetahun2@gmail.com>
2020-12-18 00:03:32 -0500

    auth

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 
 const { User } = require('./models/user');
+const user = require('./models/user');
 
-mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+mongoose.connect(config.mongoURI,
+     { useNewUrlParser: true, useUnifiedTopology: true,
+    useCreateIndex: true, useFindAndModify: false }).then(() => {
     console.log('DB connected')
 }).catch(err => console.error(err))
 
@@ -19,14 +22,42 @@ app.use(cookieParser()); //parses cookies attached to the client request object
 
 app.post('/api/users/register', (req,res) => {
     const user = new User(req.body);
-
-    user.save((err, userData) => {
-        if(err) return res.json({sucess:false, err})
-        return res.status(200).json({
-            success:true
+    
+        user.save((err, doc) => {
+            if(err) return res.json({sucess:false, err})
+            return res.status(200).json({
+                success:true,
+                userData: doc
+            })
         })
-    })
 
+})
+
+app.post('/api/user/login', (req,res) => {
+    //find the email
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user)
+            return res.json({
+                loginSuccess: false,
+                message: "Auth failed, email not found"
+            });
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch)
+                return res.json({ loginSuccess: false, message: "Wrong password" });
+
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+                res.cookie("w_authExp", user.tokenExp);
+                res
+                    .cookie("w_auth", user.token)
+                    .status(200)
+                    .json({
+                        loginSuccess: true, userId: user._id
+                    });
+            });
+        });
+    });
 })
 
 
