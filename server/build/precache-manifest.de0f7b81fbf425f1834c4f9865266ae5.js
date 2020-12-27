03e039dc75ffaaa50b66f7eda3c05cbbcfee93b2
Jonathan Getahun <kgetahun2@gmail.com>
2020-12-26 20:58:13 -0500

    move build

---
+++ server/build/precache-manifest.de0f7b81fbf425f1834c4f9865266ae5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55c4e3598389061106b11d6f876aff9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7b9e8f2093f1e8ad917",
+    "url": "/static/css/2.051905ae.chunk.css"
+  },
+  {
+    "revision": "a63e8dc3f0a54beadeef",
+    "url": "/static/css/main.b0438af6.chunk.css"
+  },
+  {
+    "revision": "c7b9e8f2093f1e8ad917",
+    "url": "/static/js/2.2a98955c.chunk.js"
+  },
+  {
+    "revision": "0f87e0cfbb145332e9889452e79883f4",
+    "url": "/static/js/2.2a98955c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a63e8dc3f0a54beadeef",
+    "url": "/static/js/main.790aee04.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  },
+  {
+    "revision": "5d686da895cc4eecb9f1f4f6ef5efcf0",
+    "url": "/static/media/featuredUser.5d686da8.jpeg"
+  }
+]);
